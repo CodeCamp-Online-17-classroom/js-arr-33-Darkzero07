19e205cd021e43177080066fbefce742cf495ab9
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-21 07:58:37 +0700

    js lab 119

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+// ให้สร้าง array result จาก array ที่กำหนด โดยใช้ arr.map(fn)
+
+const array = [
+  { name: 'apple', age: 14 },
+  { name: 'banana', age: 18 },
+  { name: 'watermelon', age: 32 }
+];
+const array2 = array.map(item => item.age )
+console.log(array2)
+// result: [14, 18, 32]
